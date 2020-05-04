c52d5569dbe5150155896e7d56e0617de7a06b4c
Jae Kim <jckim81@yahoo.com>
2020-05-04 18:56:24 -0400

    new branch deploying-a-database_connecting-to-localhost-version

--- src/config.js
+++ src/config.js
@@ -2,16 +2,3 @@ export default {
   API_ENDPOINT: `http://localhost:8000/api/bookmarks`,
   API_KEY: process.env.REACT_APP_API_KEY
 }
-
-
-/*
-export default {
-  API_ENDPOINT: `https://hidden-tundra-29574.herokuapp.com`,
-  API_KEY: process.env.REACT_APP_API_KEY
-}
-*/
-
-/*
-
-
-*/
